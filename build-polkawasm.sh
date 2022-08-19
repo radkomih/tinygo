11c4e91b3b87bf4d089d9b43d4c71f0ff4124079
Rado M <radkomih@gmail.com>
2022-08-19 23:24:12 +0300

    WIP2

--- build-polkawasm.sh
+++ build-polkawasm.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 docker build --tag polkawasm/tinygo:0.25.0 -f Dockerfile.polkawasm .
+docker run --rm -v $(pwd):/src polkawasm/tinygo:0.25.0 tinygo build -target=polkawasm -o=/src/examples/polkadot/polkadot.wasm examples/wasm/polkadot/polkadot.go
 
-docker run --rm -v $(pwd):/src polkawasm/tinygo:0.25.0 tinygo build -target=wasm -o=examples/wasm/polkadot.wasm examples/wasm/polkadot.go
+
+docker run -it -v $(pwd):/src polkawasm/tinygo:0.25.0 /bin/bash
+cd ../tinygo
+tinygo build -target=polkawasm -o=examples/wasm/polkadot.wasm examples/wasm/polkadot.go
--- examples/wasm/go.mod
+++
@@ -1,3 +0,0 @@
-module polkadot
-
-go 1.18
