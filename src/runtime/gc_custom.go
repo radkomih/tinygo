b78e694d6f812f2c1d6180d25f491cfe4a646389
Rado M <radkomih@gmail.com>
2023-07-14 12:49:02 +0300

    polkawasm: add custom gc with external allocator

--- src/runtime/gc_custom.go
+++ src/runtime/gc_custom.go
@@ -36,28 +36,161 @@ import (
 	"unsafe"
 )
 
-// initHeap is called when the heap is first initialized at program start.
-func initHeap()
+// const gcDebug = false
+
+// func printnum(num int) {
+// 	digits := [10]int{}
+
+// 	for i := 0; num > 0; i++ {
+// 		digit := num % 10
+// 		digits[i] = digit
+// 		num = num / 10
+// 	}
+
+// 	for i := 0; i < len(digits)/2; i++ {
+// 		j := len(digits) - i - 1
+// 		digits[i], digits[j] = digits[j], digits[i]
+// 	}
+
+// 	skipZeros := true
+// 	for i := 0; i < len(digits); i++ {
+// 		digit := digits[i]
+// 		if skipZeros && digit == 0 {
+// 			continue
+// 		}
+// 		skipZeros = false
+
+// 		digitStr := ""
+
+// 		switch digit {
+// 		case 0:
+// 			digitStr = "0"
+// 		case 1:
+// 			digitStr = "1"
+// 		case 2:
+// 			digitStr = "2"
+// 		case 3:
+// 			digitStr = "3"
+// 		case 4:
+// 			digitStr = "4"
+// 		case 5:
+// 			digitStr = "5"
+// 		case 6:
+// 			digitStr = "6"
+// 		case 7:
+// 			digitStr = "7"
+// 		case 8:
+// 			digitStr = "8"
+// 		case 9:
+// 			digitStr = "9"
+// 		default:
+// 		}
+
+// 		printstr(digitStr)
+// 	}
+// }
+
+// func printstr(str string) {
+// 	if !gcDebug {
+// 		return
+// 	}
+
+// 	for i := 0; i < len(str); i++ {
+// 		if putcharPosition >= putcharBufferSize {
+// 			break
+// 		}
+
+// 		putchar(str[i])
+// 	}
+// }
+
+// Total amount allocated for runtime.MemStats
+var gcTotalAlloc uint64
+
+// Total number of calls to alloc()
+var gcMallocs uint64
+
+// Total number of objected freed; for leaking collector this stays 0
+const gcFrees = 0
+
+// zeroSizedAlloc is just a sentinel that gets returned when allocating 0 bytes.
+var zeroSizedAlloc uint8
 
 // alloc is called to allocate memory. layout is currently not used.
-func alloc(size uintptr, layout unsafe.Pointer) unsafe.Pointer
+//
+//go:noinline
+func alloc(size uintptr, layout unsafe.Pointer) unsafe.Pointer {
+	// printstr("alloc(")
+	// printnum(int(size))
+	// printstr(")\n")
+
+	if size == 0 {
+		// printstr("zero-size allocation\n")
+		return unsafe.Pointer(&zeroSizedAlloc)
+	}
+
+	size = align(size)
+	gcMallocs++
+
+	// try to bound heap growth
+	if gcTotalAlloc+uint64(size) < gcTotalAlloc {
+		// printstr("\tout of memory\n")
+		abort()
+	}
+
+	// allocate memory
+	pointer := extalloc(size)
+	if pointer == nil {
+		// printstr("\textalloc call failed\n")
+		abort()
+	}
+
+	// zero-out the allocated memory
+	memzero(pointer, size)
+	gcTotalAlloc += uint64(size)
+	return pointer
+}
 
 // free is called to explicitly free a previously allocated pointer.
-func free(ptr unsafe.Pointer)
-
-// markRoots is called with the start and end addresses to scan for references.
-// It is currently only called with the top and bottom of the stack.
-func markRoots(start, end uintptr)
+func free(ptr unsafe.Pointer) {
+	extfree(ptr)
+}
 
 // GC is called to explicitly run garbage collection.
-func GC()
+func GC() {
+
+}
 
 // SetFinalizer registers a finalizer.
-func SetFinalizer(obj interface{}, finalizer interface{})
+func SetFinalizer(obj interface{}, finalizer interface{}) {
 
-// ReadMemStats populates m with memory statistics.
-func ReadMemStats(ms *MemStats)
+}
+
+// initHeap is called when the heap is first initialized at program start.
+func initHeap() {
+	// heap is initialized by the external allocator
+}
 
 func setHeapEnd(newHeapEnd uintptr) {
 	// Heap is in custom GC so ignore for when called from wasm initialization.
+}
+
+// markRoots is called with the start and end addresses to scan for references.
+// It is currently only called with the top and bottom of the stack.
+func markRoots(start, end uintptr) {
+
+}
+
+// ReadMemStats populates m with memory statistics.
+func ReadMemStats(ms *MemStats) {
+	ms.HeapIdle = 0
+	ms.HeapInuse = gcTotalAlloc
+	ms.HeapReleased = 0 // always 0, we don't currently release memory back to the OS.
+
+	ms.HeapSys = ms.HeapInuse + ms.HeapIdle
+	ms.GCSys = 0
+	ms.TotalAlloc = gcTotalAlloc
+	ms.Mallocs = gcMallocs
+	ms.Frees = gcFrees
+	ms.Sys = uint64(heapEnd - heapStart)
 }
--- src/runtime/runtime_polkawasm.go
+++ src/runtime/runtime_polkawasm.go
@@ -2,6 +2,8 @@
 
 package runtime
 
+import "unsafe"
+
 // //export _start
 // func _start() {
 // 	// These need to be initialized early so that the heap can be initialized.
@@ -10,22 +12,28 @@ package runtime
 // 	run()
 // }
 
+// // Using global variables to avoid heap allocation.
+// const putcharBufferSize = 32 * 1024
+
+// var (
+// 	putcharBuffer        = [putcharBufferSize]byte{}
+// 	putcharPosition uint = 0
+// )
+
+// //go:export _debug_buf
+// func debugBuf() uintptr {
+// 	return uintptr(unsafe.Pointer(&putcharBuffer[0]))
+// }
+
 // Abort executes the wasm 'unreachable' instruction.
 func abort() {
 	trap()
 }
 
-//go:linkname os_runtime_args os.runtime_args
-func os_runtime_args() []string {
-	return []string{}
-}
-
-//go:linkname syscall_runtime_envs syscall.runtime_envs
-func syscall_runtime_envs() []string {
-	return []string{}
-}
-
 func putchar(c byte) {
+	// putcharBuffer[putcharPosition] = c
+	// putcharPosition++
+	panic("unimplemented: putchar")
 }
 
 func getchar() byte {
@@ -55,8 +63,18 @@ func ticks() timeUnit {
 }
 
 //go:linkname now time.now
-func now() (int64, int32, int64) {
+func now() (sec int64, nsec int32, mono int64) {
 	panic("unimplemented: now")
+}
+
+//go:linkname syscall_runtime_envs syscall.runtime_envs
+func syscall_runtime_envs() []string {
+	panic("unimplemented: syscall_runtime_envs")
+}
+
+//go:linkname os_runtime_args os.runtime_args
+func os_runtime_args() []string {
+	return []string{}
 }
 
 //go:linkname syscall_Exit syscall.Exit
@@ -66,8 +84,18 @@ func syscall_Exit(code int) {
 
 //go:linkname procPin sync/atomic.runtime_procPin
 func procPin() {
+
 }
 
 //go:linkname procUnpin sync/atomic.runtime_procUnpin
 func procUnpin() {
+
 }
+
+//go:wasm-module env
+//go:export ext_allocator_malloc_version_1
+func extalloc(size uintptr) unsafe.Pointer
+
+//go:wasm-module env
+//go:export ext_allocator_free_version_1
+func extfree(ptr unsafe.Pointer)
